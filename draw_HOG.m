9f8fa608e8b86adbade7932021ac3dac721b5c77
lunaka <lucas.neto-nakadaira@ecl17.ec-lyon.fr>
2019-03-29 17:11:44 +0100

    commit 29_03

--- draw_HOG.m
+++ draw_HOG.m
@@ -1,4 +1,4 @@
-function HOG_image = draw_HOG(I, arg)
+function [HOG_image,cells] = draw_HOG(I, arg)
 
     figure;
     rgbImage = cat(3, I, I, I);
@@ -11,7 +11,7 @@ function HOG_image = draw_HOG(I, arg)
     prompt = {'Cell width (in pixels):','Cell height (in pixels):', 'Number of bins per cell:'};
     title = 'Input';
     dims = [1 35];
-    definput = {'30','30','9'};
+    definput = {'16','16','9'};
     answer = inputdlg(prompt,title,dims,definput);
     
     % Convert input to numbers
@@ -20,6 +20,12 @@ function HOG_image = draw_HOG(I, arg)
     nb_bins = str2num(answer{3});
 
     [h,w] = size(I);
+
+    nb_h = h/cell_height;
+    nb_w = w/cell_width;
+    nb_cells = ceil(nb_h)*ceil(nb_w);
+    cells = zeros(nb_cells,nb_bins);
+    counter = 1;
     
     for i = 1:cell_height:h
         for j = 1:cell_width:w
@@ -36,17 +42,19 @@ function HOG_image = draw_HOG(I, arg)
             end
   
             ax = axes('pos', [i/h j/w (limit_i-i)/h (limit_j-j)/w]);
-            fprintf("%f %f %f %f\n", i/h, j/w, (limit_i-i)/h, (limit_j-j)/w); 
-            hist1 = histogram(ax, arg(i:limit_i, j:limit_j), nb_bins, 'EdgeColor', 'none', 'FaceColor','red');
-            alpha(hist1,.7)
+            fprintf("%f %f %f %f\n", i/h, j/w, (limit_i-i)/h, (limit_j-j)/w);
+            hist1 = histogram(ax, arg(i:limit_i, j:limit_j), nb_bins, 'EdgeColor', 'none', 'FaceColor','red')
+            cells(counter,:) = hist1.Values;
+            counter = counter + 1;
+            alpha(hist1,.7);
             set(gca,'color','none')
             set(ax,'Visible','off')
             set(ax,'YTickLabel',[]);
             set(ax,'XTickLabel',[]);
-
+            
         end
     end
-    
+
     HOG_image = I;
     hold off;
 end
--- main.m
+++ main.m
@@ -25,4 +25,32 @@ subplot(2,3,2); imshow(mag); title('Magnitude');
 subplot(2,3,3); imshow(argumentPainted); title('Argument');
 subplot(2,3,[4,6]); imshow(gridImage); title('Grided Image');
 
-HOG = draw_HOG(G,arg);
+[HOG,cells] = draw_HOG(G,arg);
+
+%% Partie 2
+clear all
+close all
+
+I = imread('hog_similar.bmp');
+I1 = I(end-127:end,1:64);
+I2 = I(end-127:end,91:154);
+figure,subplot(1,2,1),imshow(I1),subplot(1,2,2),imshow(I2)
+
+[G1,O1] = gradient(I1);
+[G2,O2] = gradient(I2);
+
+[flango,h1] = draw_HOG(I1,O1);
+[flango,h2] = draw_HOG(I2,O2);
+% h1 = cell_division(I1);
+% h2 = cell_division(I2);
+h1 = double(reshape(h1,1,[]));
+h2 = double(reshape(h2,1,[]));
+
+similarity = dot(h1,h2)/(norm(h1)*norm(h2))
+
+% h1 = block_division(I1,O1);
+% h2 = block_division(I2,O2);
+% h1 = reshape(h1,1,[]);
+% h2 = reshape(h2,1,[]);
+% 
+% similarity_normalized = dot(h1,h2)/(norm(h1)*norm(h2))
